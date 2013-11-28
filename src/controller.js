fd2889b6d3953643420a5dff4da49999144a946f
Joe McCourt <joe@aviary.com>
2013-11-27 22:32:22 -0500

    Basic window GUI

--- src/controller.js
+++ src/controller.js
@@ -4,6 +4,50 @@ FS.canvasID = "#funCanvas";
 FS.dirtyCanvas = true;
 FS.lastFrameTime = 0;
 FS.mousePos = {'x':0.5,'y':0.5};
+FS.mouseState = "up";
+FS.mouseDownDelta = {'x':0,'y':0};
+FS.moduleSelected = undefined;
+
+FS.time = 0;
+
+FS.maxZIndex = 100;
+
+FS.modules = [
+	{
+		'type': 'node',
+		'box': {x:0.1,y:0.1,w:0.4,h:0.1},
+		'fillStyle': 'rgb(200,150,200)',
+		'name': 'sin(x)',
+		'eval': function(p){Math.sin(p.x);},
+		'z-index': 3
+	},
+
+	{
+		'type': 'node',
+		'box': {x:0.6,y:0.1,w:0.25,h:0.1},
+		'fillStyle': 'rgb(150,150,50)',
+		'name': 'cos(x)',
+		'eval': function(p){Math.cos(p.x);},
+		'z-index': 0
+	},
+
+	{
+		'type': 'node',
+		'box': {x:0.4,y:0.8,w:0.25,h:0.1},
+		'fillStyle': 'rgb(50,150,150)',
+		'name': 'PI',
+		'eval': function(){Math.PI;},
+		'z-index': 2
+	},
+
+	{
+		'type': 'funDisplay',
+		'box': {x:0.25,y:0.25,w:0.5,h:0.5},
+		'name': 'demoFunction',
+		'eval': function(params){return params.amp*Math.sin(params.x*Math.PI*2*params.freq+params.t);},
+		'z-index': 1
+	}
+];
 
 FS.main = function() {
 	FS.startSession();
@@ -17,21 +61,25 @@ FS.startSession = function() {
 	FS.canvas = $(FS.canvasID)[0];
 	FS.ctx = FS.canvas.getContext("2d");
 
-	// FS.setLevelRenderBox();
+	//FS.setLevelRenderBox();
 	// FS.loadGameState();
 	FS.resizeToFit();
+
+	FS.reorderModules();
 
 	FS.dirtyCanvas = true;
 	FS.initEvents();
 };
 
+
 FS.gameLoop = function(time) {
 	var ctx = FS.ctx;
+	FS.time = time;
 
 	if(FS.dirtyCanvas){
 		// FS.dirtyCanvas = false;
 		FS.drawClear();
-		FS.drawFun(time);
+		FS.drawModules();
 	}
 
 	requestNextAnimationFrame(FS.gameLoop);
@@ -40,12 +88,56 @@ FS.gameLoop = function(time) {
 	FS.lastFrameTime = time;
 };
 
+FS.reorderModules = function() {
+	FS.modules.sort(function(a, b) {
+		return a['z-index'] - b['z-index'];
+	});
+};
+
 FS.mousemove = function(x,y){
 	var w = FS.canvas.width;
 	var h = FS.canvas.height;
 
-	FS.mousePos = {'x':x/w,'y':y/h};
-}
+	FS.mousePos = {'x':x,'y':y};
+
+	if(FS.mouseState === "down"){
+		var d = FS.mouseDownDelta;
+		var m = FS.moduleSelected;
+		if(m && d){
+			m.box.x = x-d.x;
+			m.box.y = y-d.y;
+		}
+	}
+};
+
+FS.mousedown = function(x,y){
+	FS.mousePos = {'x':x,'y':y};
+	FS.mouseState = "down";
+
+	FS.moduleSelected = undefined;
+	for(var i = 0; i < FS.modules.length; i++){
+		var m = FS.modules[i];
+		var b = m.box;
+
+		if(b.x <= x && b.x+b.w >= x && b.y <= y && b.y+b.h >= y){
+			if(!FS.moduleSelected || FS.moduleSelected['z-index'] < m['z-index']){
+				FS.moduleSelected = FS.modules[i];
+				FS.mouseDownDelta = {x:x-b.x,y:y-b.y};
+			}
+		}
+	}
+
+	if(FS.moduleSelected) {
+		FS.moduleSelected['z-index'] = FS.maxZIndex;
+		FS.maxZIndex++;
+		FS.reorderModules(); //Inefficeint, but whatever
+	}
+};
+
+FS.mouseup = function(x,y){
+	FS.mousePos = {'x':x,'y':y};
+	FS.mouseState = "up";
+};
 
 FS.resizeToFit = function(){
 	var w = $(window).width();
@@ -63,13 +155,38 @@ FS.initEvents = function(){
 		FS.resizeToFit();
 	});
 
-	$(window).mousemove(function (e) {
-		var offset = $(FS.canvasID).offset();
-		var x = e.pageX - offset.left;
-		var y = e.pageY - offset.top;
+    $(window).mousemove(function (e) {
+        var offset = $(FS.canvasID).offset();
+        var x = e.pageX - offset.left;
+        var y = e.pageY - offset.top;
 
-		FS.mousemove(x,y);
-	});
+		var w = FS.canvas.width;
+		var h = FS.canvas.height;
+
+        FS.mousemove(x/w,y/h);
+    });
+
+    $(window).mousedown(function (e) {
+        var offset = $(FS.canvasID).offset();
+        var x = e.pageX - offset.left;
+        var y = e.pageY - offset.top;
+
+		var w = FS.canvas.width;
+		var h = FS.canvas.height;
+
+        FS.mousedown(x/w,y/h);
+    });
+
+    $(window).mouseup(function (e) {
+        var offset = $(FS.canvasID).offset();
+        var x = e.pageX - offset.left;
+        var y = e.pageY - offset.top;
+
+		var w = FS.canvas.width;
+		var h = FS.canvas.height;
+
+        FS.mouseup(x/w,y/h);
+    });
 };
 
 // *** LocalStorage Check ***
--- src/view.js
+++ src/view.js
@@ -1,9 +1,11 @@
+FS.funBox = {x:0.25,y:0.25,w:0.5,h:0.5};
+
 FS.drawClear = function(){
 	var ctx = FS.ctx;
 	ctx.clearRect(0,0,FS.canvas.width,FS.canvas.height);
 };
 
-FS.drawFun = function(time) {
+FS.drawFun = function(module) {
 	var ctx = FS.ctx;
 	ctx.save();
 
@@ -16,26 +18,83 @@ FS.drawFun = function(time) {
 	var n = w;
 	var inputData = {
 		'scale': 'linear',
-		'variables': {'x':0,'t':time/1000,'amp':y,'freq':10*x},
+		'variables': {'x':0,'t':FS.time/1000,'amp':y,'freq':10*x},
 		'independentVariable': 'x',
 		'range': [0,1],
 		'numPoints': n
 	};
 
 	// var fun = FS.generateFun("Math.cos(Math.PI*2*4*%x)-0.1*%t");
+	var outData = FS.getFunValues(module, inputData);
 
-	var outData = FS.getFunValues(FS.fun,inputData);
-
-	ctx.lineWidth = 3;
+	ctx.lineWidth = 2;
 	ctx.fillStyle = 'black';
 	ctx.joinCap = 'round';
-	ctx.beginPath();
+
+	ctx.fillStyle = 'rgba(100,200,100,0.6)';
 	
+	var b = module.box;
+	ctx.beginPath();
+	ctx.lineTo(b.x*w,b.y*h);
+	ctx.lineTo((b.x+b.w)*w,b.y*h);
+	ctx.lineTo((b.x+b.w)*w,(b.y+b.h)*h);
+	ctx.lineTo(b.x*w,(b.y+b.h)*h);
+	ctx.closePath();
+	ctx.stroke();
+	ctx.fill();
+	ctx.beginPath();
+
 	for(var i = 0; i < n; i++){
-		ctx.lineTo(i*w/n, h*(-0.5*outData[i]+0.5));
+		var drawX = (i/n*b.w + b.x)*w;
+		var drawY = ((-0.5*outData[i]+0.5)*b.h + b.y)*h;
+
+		ctx.lineTo(drawX, drawY);
 	}
 
 	ctx.stroke();
+	ctx.restore();
+};
+
+FS.drawNode = function(module) {
+	var ctx = FS.ctx;
+	ctx.save();
+
+	var w = FS.canvas.width;
+	var h = FS.canvas.height;
+
+	var b = module.box;
+
+	ctx.fillStyle = module.fillStyle;
+
+	ctx.beginPath();
+	ctx.lineTo(b.x*w,b.y*h);
+	ctx.lineTo((b.x+b.w)*w,b.y*h);
+	ctx.lineTo((b.x+b.w)*w,(b.y+b.h)*h);
+	ctx.lineTo(b.x*w,(b.y+b.h)*h);
+	ctx.closePath();
+	ctx.stroke();
+	ctx.fill();
+
+
+	var height = 0.4*Math.min(w*b.w,h*b.h);
+	ctx.textAlign = "center";
+	ctx.textBaseline = "middle";
+	ctx.font = height + "px Lucida Console";
+	ctx.fillStyle = 'black';
+	ctx.fillText(module.name,(b.x+b.w/2)*w,(b.y+b.h/2)*h);
 
 	ctx.restore();
+};
+
+FS.drawModules = function() {
+
+	for(var i = 0; i < FS.modules.length; i++){
+		var m = FS.modules[i];
+
+		if(m.type == 'node'){
+			FS.drawNode(m);
+		}else if(m.type == 'funDisplay'){
+			FS.drawFun(m);
+		}
+	}
 };
